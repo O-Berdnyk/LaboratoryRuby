38f134b48cd87a1749b70540ebb03688b9d29363
O-Berdnyk <berdnyk.oleksandr@chnu.edu.ua>
2024-11-10 13:47:28 +0200

    create parser class

--- lib/main.rb
+++ lib/main.rb
@@ -2,6 +2,7 @@ require_relative 'config'
 require_relative 'logger'
 require_relative 'item'
 require_relative 'item_collection'
+require_relative 'website_parser'
 
 # Ініціалізація екземпляра AppConfigLoader
 config_loader = AppConfigLoader.new
@@ -14,15 +15,15 @@ config_loader.load_libs(system_libs, libs_dir)
 main_config_file = 'config/default_config.yaml' # Шлях до основного конфігураційного файлу
 additional_configs_dir = 'config/additional_configs' # Директорія з додатковими конфігураціями
 
-config_loader.config(main_config_file, additional_configs_dir)
+config  = config_loader.config(main_config_file, additional_configs_dir)
 
 # Перевірка завантаження конфігурацій - виведення у форматі JSON
 config_loader.pretty_print_config_data
 
 logging_config = config_loader.load_logging_config('config/additional_configs/logging_config.yaml')
 
-# Ініціалізуємо логер
-# MyApplicationBerdnyk::LoggerManager.initialize_logger(logging_config)
+#Ініціалізуємо логер
+MyApplicationBerdnyk::LoggerManager.initialize_logger(logging_config)
 
 # itemCollection = MyApplicationBerdnyk::ItemCollection.new
 # itemCollection.generate_test_items(5) # Генеруємо 5 тестових товарів
@@ -46,3 +47,5 @@ puts configurator.config
 # Вивід доступних конфігураційних ключів
 puts MyApplicationBerdnyk::Configurator.available_methods
 
+parser = MyApplicationBerdnyk::SimpleWebsiteParser.new(config)
+parser.start_parse
